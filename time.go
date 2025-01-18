4db051abfcb7ab4baaa9f7fc65b1ace35435a4e5
mrvtornik <nick.t@getblock.io>
2025-01-18 03:53:53 +0300

    update

--- time.go
+++ time.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"flag"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"regexp"
 	"strconv"
@@ -24,7 +24,7 @@ var (
 	between8000and9000ms, between9000and10000ms, between10000and11000ms, between11000and12000ms, between12000and13000ms, between13000and14000ms,
 	between14000and15000ms, between15000and16000ms, between16000and17000ms, between17000and18000ms, between18000and19000ms, between19000and20000ms,
 	morethan20000ms int
-	allSizeFile, less1mb, between1mband5mb, between5mband10mb, more10bm, size0, size int
+	size, allSizeFile, less1mb, between1mband5mb, between5mband10mb, more10bm, size0 int
 	allResponceTime                                                                  time.Duration
 	avarageTime                                                                      time.Duration
 	mutex                                                                            sync.Mutex
@@ -45,7 +45,7 @@ func makePostRequest(url string, jsonData string, wg *sync.WaitGroup) {
 		fmt.Printf("Error creating request: %v\n", err)
 		return
 	}
-	body, err := ioutil.ReadAll(req.Body)
+	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		fmt.Printf("client: error reading response body: %s\n", err)
 		return
@@ -192,6 +192,7 @@ func makeWssRequest(url string, jsonData string, wg *sync.WaitGroup) {
 	fmt.Printf("%+v\n", jsonData)
 	defer conn.Close()
 }
+
 func main() {
 	flag.StringVar(&jsonData, "d", `{"jsonrpc": "2.0","method": "eth_blockNumber","params": [],"id": "getblock.io"}`, "Request you want to use")
 	flag.StringVar(&url, "u", "https://go.getblock.io/c0f16139fdfe439eaab426f21dc94e70", "url")
@@ -239,25 +240,25 @@ func main() {
 	fmt.Printf("\033[32mBetween 300 and 400 ms: %d\n", between300and400ms)
 	fmt.Printf("\033[32mBetween 400 and 500 ms: %d\n", between400and500ms)
 	fmt.Printf("\033[32mBetween 500 and 1000 ms: %d\n", between500and1000ms)
-	fmt.Printf("\033[33mBetween 1000 and 2000 ms: %d\n", between1000and2000ms)
-	fmt.Printf("\033[33mBetween 2000 and 3000 ms: %d\n", between2000and3000ms)
-	fmt.Printf("\033[33mBetween 3000 and 4000 ms: %d\n", between3000and4000ms)
-	fmt.Printf("\033[33mBetween 4000 and 5000 ms: %d\n", between4000and5000ms)
-	fmt.Printf("\033[33mBetween 5000 and 6000 ms: %d\n", between5000and6000ms)
-	fmt.Printf("\033[33mBetween 6000 and 7000 ms: %d\n", between6000and7000ms)
-	fmt.Printf("\033[33mBetween 7000 and 8000 ms: %d\n", between7000and8000ms)
-	fmt.Printf("\033[33mBetween 8000 and 9000 ms: %d\n", between8000and9000ms)
-	fmt.Printf("\033[33mBetween 9000 and 10000 ms: %d\n", between9000and10000ms)
-	fmt.Printf("\033[33mBetween 10000 and 11000 ms: %d\n", between10000and11000ms)
-	fmt.Printf("\033[33mBetween 11000 and 12000 ms: %d\n", between11000and12000ms)
-	fmt.Printf("\033[33mBetween 12000 and 13000 ms: %d\n", between12000and13000ms)
-	fmt.Printf("\033[33mBetween 13000 and 14000 ms: %d\n", between13000and14000ms)
-	fmt.Printf("\033[33mBetween 14000 and 15000 ms: %d\n", between14000and15000ms)
-	fmt.Printf("\033[33mBetween 15000 and 16000 ms: %d\n", between15000and16000ms)
-	fmt.Printf("\033[33mBetween 16000 and 17000 ms: %d\n", between16000and17000ms)
-	fmt.Printf("\033[33mBetween 17000 and 18000 ms: %d\n", between17000and18000ms)
-	fmt.Printf("\033[33mBetween 18000 and 19000 ms: %d\n", between18000and19000ms)
-	fmt.Printf("\033[33mBetween 19000 and 20000 ms: %d\n", between19000and20000ms)
+	fmt.Printf("\033[31mBetween 1000 and 2000 ms: %d\n", between1000and2000ms)
+	fmt.Printf("\033[31mBetween 2000 and 3000 ms: %d\n", between2000and3000ms)
+	fmt.Printf("\033[31mBetween 3000 and 4000 ms: %d\n", between3000and4000ms)
+	fmt.Printf("\033[31mBetween 4000 and 5000 ms: %d\n", between4000and5000ms)
+	fmt.Printf("\033[31mBetween 5000 and 6000 ms: %d\n", between5000and6000ms)
+	fmt.Printf("\033[31mBetween 6000 and 7000 ms: %d\n", between6000and7000ms)
+	fmt.Printf("\033[31mBetween 7000 and 8000 ms: %d\n", between7000and8000ms)
+	fmt.Printf("\033[31mBetween 8000 and 9000 ms: %d\n", between8000and9000ms)
+	fmt.Printf("\033[31mBetween 9000 and 10000 ms: %d\n", between9000and10000ms)
+	fmt.Printf("\033[31mBetween 10000 and 11000 ms: %d\n", between10000and11000ms)
+	fmt.Printf("\033[31mBetween 11000 and 12000 ms: %d\n", between11000and12000ms)
+	fmt.Printf("\033[31mBetween 12000 and 13000 ms: %d\n", between12000and13000ms)
+	fmt.Printf("\033[31mBetween 13000 and 14000 ms: %d\n", between13000and14000ms)
+	fmt.Printf("\033[31mBetween 14000 and 15000 ms: %d\n", between14000and15000ms)
+	fmt.Printf("\033[31mBetween 15000 and 16000 ms: %d\n", between15000and16000ms)
+	fmt.Printf("\033[31mBetween 16000 and 17000 ms: %d\n", between16000and17000ms)
+	fmt.Printf("\033[31mBetween 17000 and 18000 ms: %d\n", between17000and18000ms)
+	fmt.Printf("\033[31mBetween 18000 and 19000 ms: %d\n", between18000and19000ms)
+	fmt.Printf("\033[31mBetween 19000 and 20000 ms: %d\n", between19000and20000ms)
 	fmt.Printf("\033[31mMore than 20000 ms: %d\n", morethan20000ms)
 	fmt.Printf("\033[32mAvarage response time: %v\n", avarageTime)
 	fmt.Printf("\033[32mLess than 1mb: %d\n", less1mb)
