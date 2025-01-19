50ca2ab0b4b114920f762bb6814916e25514f03f
MrVt0rN1k <mrvtorn1k2354@gmail.com>
2025-01-19 03:09:05 +0300

    refactor v2

--- makePostRequest.go
+++ makePostRequest.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"io"
 	"net/http"
-	"regexp"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -14,53 +11,31 @@ import (
 func makePostRequest(url string, jsonData string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	startTime := time.Now()
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(jsonData)))
+
+	req, err := createRequest(url, jsonData)
 	if err != nil {
-		fmt.Printf("Error creating request: %v\n", err)
+		fmt.Println(err)
 		return
 	}
+
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		fmt.Printf("client: error reading response body: %s\n", err)
 		return
 	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept-Encoding", "gzip")
+
 	client := &http.Client{}
-
-	resp, err := client.Do(req)
+	resp, err := executeRequest(client, req)
 	if err != nil {
-		fmt.Printf("Error making request: %v\n", err)
+		fmt.Println(err)
 		return
 	}
-
 	defer resp.Body.Close()
-	contentLength := resp.Header.Get("Content-Length")
 
-	length, err := strconv.Atoi(contentLength)
-	if err != nil {
-		fmt.Println("Error converting Content-Length to integer:", err)
+	if err := processResponse(resp, body); err != nil {
+		fmt.Println(err)
 		return
 	}
 
-	fileSize(length)
-
-	lookFor := "code"
-	refString := string(body)
-	pattern := regexp.MustCompile(`\b` + regexp.QuoteMeta(lookFor) + `\b`)
-	if pattern.MatchString(refString) {
-		countError++
-	}
-
-	statusCode(resp)
-	endTime := time.Now()
-	responseTime := endTime.Sub(startTime)
-	allResponceTime += responseTime
-	mutex.Lock()
-	responseCounter++
-	avarageTime = allResponceTime / time.Duration(responseCounter)
-	fmt.Printf("\r\033[1;36mWorking time: %v ", allResponceTime/time.Duration(requestsPerSecond))
-	allSizeFile += size
-	switchTime(responseTime)
-	mutex.Unlock()
+	updateStats(startTime)
 }
